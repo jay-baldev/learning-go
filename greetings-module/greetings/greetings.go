475c9667c64530a2000db5b517f7acace662eb88
jay-baldev <baldevjay@gmail.com>
2022-07-31 14:46:33 +0530

    add error handling

--- greetings-module/greetings/greetings.go
+++ greetings-module/greetings/greetings.go
@@ -1,11 +1,19 @@
 package greetings
 
-import "fmt"
+import( 
+    "fmt"
+    "errors"
+}
 
 // Hello returns a greeting for the named person
 
-func Hello(name string) string {
-    //Return a greeting that embeds the name in the message
+func Hello(name string) (string, error) {
+    // If no name was given return an error message
+    if name == "" {
+        return "", errors.New("empty name")
+    }
+    // If a name was received, return a greeting 
+    // that embeds the name in the message
     message := fmt.Sprintf("Namaste %v, padhariye!", name)
-    return message
+    return message, nil
 }
