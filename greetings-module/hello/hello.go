e7142a72e745554d5a0c9e0c29ec74c1d039e876
jay-baldev <baldevjay@gmail.com>
2022-07-31 15:41:57 +0530

    Return greetings for multiple people

--- greetings-module/hello/hello.go
+++ greetings-module/hello/hello.go
@@ -14,15 +14,17 @@ func main() {
     log.SetPrefix("greetings: ")
     log.SetFlags(0)
 
-    // Request a greeting message.
-    message, err := greetings.Hello("Jay")
+    // A slice of names.
+    names := []string{"Chandragupt", "Bindusar", "Ashok"}
+    // Request a greeting messages.
+    messages, err := greetings.Hellos(names)
     // If an error was returned, print it to the console and
     // exit the program
     if err != nil {
         log.Fatal(err)
     }
 
-    // If no error was returned, print the returned message
-    // to the console
-    fmt.Println(message)
+    // If no error was returned, print the returned map of
+    // messages to the console
+    fmt.Println(messages)
 }
